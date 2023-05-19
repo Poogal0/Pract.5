bad0a989ddcc448532dd850f3b77f237d7259cbc
Poogal0 <golubchikov_2002@mail.ru>
2023-05-19 18:10:03 +0400

    v2 - refact

---
+++ LogElement.cpp
@@ -0,0 +1,6 @@
+#include "LogElement.h"
+LogElement::LogElement(int in1, int in2) {
+    input1 = in1;
+    input2 = in2;
+    output = (in1 && in2);
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-class LogElement {
-public:
-    int input1;
-    int input2;
-    int output;
-    LogElement(int in1, int in2, int out) : input1(in1), input2(in2), output(out) {}
-};
+#include "LogElement.h"
+
 int main() {
-    LogElement elem(1, 0, 0);
+    LogElement elem(1, 1);
     std::cout << "Input 1: " << elem.input1 << std::endl;
     std::cout << "Input 2: " << elem.input2 << std::endl;
     std::cout << "Output: " << elem.output << std::endl;
+
     return 0;
 }
